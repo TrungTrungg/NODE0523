54c81e8d556f54b723aefb126e5a862de8a86b34
TrungTrungg <xuantrung97tn@gmail.com>
2023-08-04 17:02:20 +0700

    04/08

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,7 @@ const router = express.Router();
 const frontendRouter = require('./frontend');
 const backendRouter = require('./backend');
 
-const apiRouter = require('./api');
-
 router.use('/admin', backendRouter);
 router.use('/', frontendRouter);
-
-router.use('/api', apiRouter);
 
 module.exports = router;
